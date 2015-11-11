2b0d6e27d87a876d3f95db4d1776487bbc7ccebd
haresh-chudgar <haresh.chudgar@gmail.com>
2015-11-11 00:30:58 -0500

    Minor bug fixes
    7 movies don't have a year of release, and a few ratings may not have
    the date of rating. Not sure about the latter.
    Modified code to deal with that. The total size of 18 files comes up to
    2.99 GB.

--- datafusion.py
+++ datafusion.py
@@ -2,50 +2,40 @@ __author__ = 'haresh'
 
 import time
 import datetime
-import os
 import csv
-import pandas as pd
-from sklearn.linear_model import LogisticRegression
-from sklearn.cross_validation import train_test_split
-from sklearn import metrics
-from sklearn.cross_validation import cross_val_score
-from patsy import dmatrices
 
-numberOfMovies = 10
+numberOfMovies = 17770
 movieRatings = []
 movieInfo = {}
 with open('data\movie_titles.txt', mode='r') as infile:
     reader = csv.reader(infile)
     movieInfo = dict((rows[0],rows[1]) for rows in reader)
 
-for subdir, dirs, files in os.walk("data/training_set"):
-    for filePath in files:
-        with open(os.path.join(subdir, filePath)) as movieFile:
-            currentMovie = 0
-            for line in movieFile:
-                if(line.__contains__(":")):
-                    splitStrings = line.split(":")
-                    currentMovie = int(splitStrings[0])
+for currentMovie in range(1,numberOfMovies + 1):
+    filePath = 'data/training_set/mv_%07d.txt' % currentMovie
+    with open(filePath) as movieFile:
+        currentMovie = 0
+        for line in movieFile:
+            if(line.__contains__(":")):
+                splitStrings = line.split(":")
+                currentMovie = int(splitStrings[0])
+            else:
+                splitStrings = line.split(",")
+                userId = int(splitStrings[0])
+                userRating = int(splitStrings[1])
+                splitStrings[2] = splitStrings[2].split("\n")[0]
+                if(splitStrings[2] == "NULL"):
+                    dateOfRating = -1
                 else:
-                    splitStrings = line.split(",")
-                    userId = int(splitStrings[0])
-                    userRating = int(splitStrings[1])
-                    splitStrings[2] = splitStrings[2].split("\n")[0]
                     dateOfRating = int(time.mktime(datetime.datetime.strptime(splitStrings[2], "%Y-%m-%d").timetuple()))
-                    mId = currentMovie.__str__()
+                mId = currentMovie.__str__()
+                if(movieInfo[currentMovie.__str__()] == "NULL"):
+                    dateOfRelease = -1
+                else:
                     dateOfRelease = int(movieInfo[currentMovie.__str__()])
-                    movieRatings.append([currentMovie, userId, userRating, dateOfRating, dateOfRelease])
-        print(filePath, movieRatings.__len__())
-        if(currentMovie % 1000 == 0):
-            with open("consolidatedRatings_" + currentMovie.__str__() +  ".csv", "wb") as csvFile:
-                writer = csv.writer(csvFile,delimiter=',')
-                writer.writerows(movieRatings)
-            movieRatings = []
-
-df_ = pd.DataFrame(movieRatings, columns=['movieid', 'userid', 'rating', 'ratingdate', 'releasedate'])
-y, X = dmatrices('rating ~ movieid + userid + ratingdate + releasedate', df_, return_type="dataframe")
-print(X)
-print(y)
-
-
-
+                movieRatings.append([currentMovie, userId, userRating, dateOfRating, dateOfRelease])
+    print(filePath, movieRatings.__len__())
+    if(currentMovie % 1000 == 0 or currentMovie == numberOfMovies):
+        with open("consolidatedRatings_" + currentMovie.__str__() +  ".csv", "wb") as csvFile:
+            writer = csv.writer(csvFile,delimiter=',')
+            writer.writerows(movieRatings)
